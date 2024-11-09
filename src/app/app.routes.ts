0eb8dd0c6630a70c39a487cfa61321feb880c423
JAY TAILOR <tailorj64@gmail.com>
2024-11-09 20:50:14 +0530

    Init structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { SurveyComponent } from './components/survey/survey.component';
+import { FillSurveyComponent } from './components/fill-survey/fill-survey.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'survey',
+  },
+  {
+    path: 'survey',
+    component: SurveyComponent,
+  },
+  {
+    path: 'fill-survey',
+    component: FillSurveyComponent,
+  },
+];
---
+++ src/app/components/fill-survey/fill-survey.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fill-survey',
+  standalone: true,
+  imports: [],
+  templateUrl: './fill-survey.component.html',
+  styleUrl: './fill-survey.component.scss'
+})
+export class FillSurveyComponent {
+
+}
