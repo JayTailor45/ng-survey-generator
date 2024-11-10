d5fc0a5d3cb9eb464bd2357c4c4e1369ae02f8d2
JAY TAILOR <tailorj64@gmail.com>
2024-11-10 16:49:48 +0530

    add proper routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { SurveyComponent } from './components/survey/survey.component';
 import { FillSurveyComponent } from './components/fill-survey/fill-survey.component';
+import { LogicComponent } from './components/logic/logic.component';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,10 @@ export const routes: Routes = [
   {
     path: 'survey',
     component: SurveyComponent,
+  },
+  {
+    path: 'logic',
+    component: LogicComponent,
   },
   {
     path: 'fill-survey',
